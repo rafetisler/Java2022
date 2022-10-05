232210194296ea2ea0a1696810e22817ae396284
Rafet İşler <rafetisler@gmail.com>
2022-10-05 18:13:49 +0300

    Initial commit

---
+++ 1week/src/mukemmelSayi.java
@@ -0,0 +1,22 @@
+
+public class mukemmelSayi {
+
+	public static void main(String[] args) {
+		int number=125;
+		int total =0;
+		
+		for(int i=1;i<number;i++) {
+			if(number% i==0) {
+				total=total+i;
+			}
+		}
+		
+		if(total==number) {
+			System.out.println("Mükemmel sayýdýr.");
+		}else {
+			System.out.println("Mükemmel sayý deðildir.");
+		}
+
+	}
+
+}
---
+++ 1week/src/sesliHarfler.java
@@ -0,0 +1,21 @@
+
+public class sesliHarfler {
+
+	public static void main(String[] args) {
+		char harf = 'A';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalý sesli harf");
+			break;
+		default:
+			System.out.println("Ýnce Sesli harf");
+
+		}
+
+	}
+
+}
