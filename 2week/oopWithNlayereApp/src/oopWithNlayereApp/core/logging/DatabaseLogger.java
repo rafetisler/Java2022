b88d8ea92e98eb7329c4bda54f73cdd44f9cef59
Rafet İşler <rafetisler@gmail.com>
2022-10-05 23:19:06 +0300

    Up

---
+++ 2week/oopWithNlayereApp/src/oopWithNlayereApp/core/logging/DatabaseLogger.java
@@ -0,0 +1,9 @@
+package oopWithNlayereApp.core.logging;
+
+public class DatabaseLogger implements Logger{
+    @Override
+    public void log(String data) {
+        System.out.println("Veri Tabanına Loglandı : +"+ data);
+
+    }
+}
