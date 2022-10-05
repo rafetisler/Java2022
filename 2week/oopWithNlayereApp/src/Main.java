5bfc1e5ba04ffc75df1d50d6046aeb685e3b3cb9
Rafet İşler <rafetisler@gmail.com>
2022-10-05 23:45:57 +0300

    Up

--- 2week/oopWithNlayereApp/src/Main.java
+++ 2week/oopWithNlayereApp/src/Main.java
@@ -1,12 +1,19 @@
 import oopWithNlayereApp.business.ProductManager;
+import oopWithNlayereApp.core.logging.DatabaseLogger;
+import oopWithNlayereApp.core.logging.FileLogger;
+import oopWithNlayereApp.core.logging.Logger;
 import oopWithNlayereApp.dataAccess.HibernateProductDao;
 import oopWithNlayereApp.dataAccess.JdbcProductDao;
 import oopWithNlayereApp.entities.Product;
 
+
+
 public class Main {
     public static void main(String[] args) throws  Exception{
         Product product1 =new Product(1,"İphone",9999);
-        ProductManager productManager=new ProductManager( new HibernateProductDao());
+        Logger[]loggers ={new DatabaseLogger(),new FileLogger()};
+
+        ProductManager productManager=new ProductManager( new HibernateProductDao(), loggers);
         productManager.add(product1);
 
 
--- 2week/oopWithNlayereApp/src/oopWithNlayereApp/business/ProductManager.java
+++ 2week/oopWithNlayereApp/src/oopWithNlayereApp/business/ProductManager.java
@@ -1,26 +1,39 @@
 package oopWithNlayereApp.business;
 
+import oopWithNlayereApp.core.logging.Logger;
 import oopWithNlayereApp.dataAccess.HibernateProductDao;
 import oopWithNlayereApp.dataAccess.JdbcProductDao;
 import oopWithNlayereApp.dataAccess.ProductDao;
 import oopWithNlayereApp.entities.Product;
 
-public class ProductManager {
-    private  ProductDao productDao;
+import java.util.List;
 
-    public ProductManager(ProductDao productDao) {
-        this.productDao = productDao;
+public class ProductManager {
+    private ProductDao productDao;
+    private Logger[] loggers;
+
+    public ProductManager(ProductDao productDao, Logger[] loggers) {
+
     }
 
-    public  void add(Product product) throws Exception {
+    {
+        this.productDao = productDao;
+        this.loggers = loggers;
+    }
+
+    public void add(Product product) throws Exception {
         //response request
         // İş Kurlları
-        if(product.getUnitPrice()<10){
+        if (product.getUnitPrice() < 10) {
             throw new Exception("Ürün Fiyatı 10 dan Küçük Olamaz.");
         }
-
-        //ProductDao productDao=new HibernateProductDao();
         productDao.add(product);
+        for (Logger logger: loggers
+             ) {
+            logger.log(product.getName());
+
+        }
+
 
     }
 }
